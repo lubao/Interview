dcc5fcfb45d22acefe79a1ceed22c0f894cc13fb
paul <paulcoffee@gmail.com>
2011-06-03 04:00:40 +0000

    add Tree.py

--- python/tree/Tree.py
+++ python/tree/Tree.py
@@ -29,38 +29,18 @@ class Tree:
             data.sort()
         self.generate_bst_from_sorted_array(data, self.root, rate-1, 0)
 
-    def generate_bst_from_sorted_array(self, data, current, upper, lower):
-        #if lower > upper : return
+    def generate_bst_from_sorted_array(self, data, 
+                current, upper=999, lower=0):
+        if lower > upper : return
         mid = lower + (upper-lower)/2
         current.value = data[mid]
         current.left = Node()
         current.right = Node()
-        if lower <= mid - 1:
-            self.generate_bst_from_sorted_array(
-                data, current.left, mid-1, lower 
-            )
-        if mid + 1 <= upper:
-            self.generate_bst_from_sorted_array(
-                data, current.right, upper, mid+1 
-            )
-
-    def generate_list_for_bst(self):
-        ret = []
-        queue = [self.root]
-        while queue.__len__() > 0:
-            working_nodes = []
-            res = []
-            while queue.__len__() > 0 :
-                working_nodes.append(queue.pop(0))
-            while working_nodes.__len__() > 0:
-                working_node = working_nodes.pop(0)
-                if working_node.value is not None:
-                    res.append(working_node.value)
-                if working_node.get_left() is not None : 
-                    queue.append(working_node.get_left())
-                if working_node.get_right() is not None:
-                    queue.append(working_node.get_right())
-            ret.append(res)
-        return ret
-
-            
+        self.generate_bst_from_sorted_array(
+            data, current.left, mid-1, lower 
+        )
+        self.generate_bst_from_sorted_array(
+            data, current.right, upper, mid+1 
+        )
+        return self.root
+        
